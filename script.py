0a439eed3b1df7a425f838ce5585dedf7673ad83
kassanj <kassandram022@gmail.com>
2016-10-23 13:31:33 -0400

    extract the number of suspension by year, set teams and games, and create a Suspension class

--- script.py
+++ script.py
@@ -16,3 +16,38 @@ for row in nfl_suspensions:
         years[row_year] = 1
 
 print(years)
+
+
+team = [row[1] for row in nfl_suspensions]
+games = [row[2] for row in nfl_suspensions]   
+    
+unique_teams = set(team)
+unique_games = set(games)
+    
+print(unique_teams)
+print(unique_games)
+
+
+class Suspension():
+    def __init__(self, row):
+        self.name = row[0]
+        self.team = row[1]
+        self.games = row[2]
+        self.year = row[5]
+   
+third_suspension = Suspension(nfl_suspensions[2])   
+
+
+class Suspension():
+    def __init__(self,row):
+        self.name = row[0]
+        self.team = row[1]
+        self.games = row[2]    
+        try:
+            self.year = int(row[5])
+        except Exception:
+            self.year = 0   
+    def get_year(self):
+        return(self.year)
+    
+missing_year = Suspension(nfl_suspensions[22])
